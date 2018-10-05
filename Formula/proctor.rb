52f3d155293332057a0d3c75e957aae806811782
Praveen Shukla <praveen.shukla@go-jek.com>
2018-10-05 18:49:59 +0530

    [TravisCI] updating brew formula for release v0.2.0

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -1,7 +1,8 @@
 class Proctor < Formula
   desc "Proctor CLI"
   homepage "https://github.com/gojektech/proctor"
-  url "https://github.com/gojektech/proctor/releases/download/v0.1.0/proctor_0.1.0_Darwin_x86_64.tar.gz"
+  url "https://github.com/gojektech/proctor/releases/download/v0.2.0/proctor_0.2.0_Darwin_x86_64.tar.gz"
+  sha256 "0b4fe78774e1ae1b208f7c3098c4e623cab55445d804d91e2e935eaf47d332e5"
   head "https://github.com/gojektech/proctor.git"
 
   def install
