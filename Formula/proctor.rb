6555d05283cd79af7acf53600feac7c459515da9
Praveen Shukla <praveen.shukla@go-jek.com>
2018-10-09 12:03:41 +0530

    Downgrade brew formula for proctor release to v0.1.0

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -1,8 +1,8 @@
 class Proctor < Formula
   desc "Proctor CLI"
   homepage "https://github.com/gojektech/proctor"
-  url "https://github.com/gojektech/proctor/releases/download/v0.2.0/proctor_0.2.0_Darwin_x86_64.tar.gz"
-  sha256 "0b4fe78774e1ae1b208f7c3098c4e623cab55445d804d91e2e935eaf47d332e5"
+  url "https://github.com/gojektech/proctor/releases/download/v0.1.0/proctor_0.1.0_Darwin_x86_64.tar.gz"
+  sha256 "a6190559dcb12cdb23be5fd635ccb7037e5e2e2a8c2c359941b9feb59f026c90"
   head "https://github.com/gojektech/proctor.git"
 
   def install
