bd71230fb3c72ce09d6fb5f64d3128e3432a24bc
akshat <olttwa@users.noreply.github.com>
2018-11-22 11:57:49 +0530

    [Roy|Akshat] Manually update proctor formula
    - Automated release failed in CI because of auth issue

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -1,9 +1,9 @@
 class Proctor < Formula
   desc "Proctor CLI"
   homepage "https://github.com/gojektech/proctor"
-  url "https://github.com/gojektech/proctor/releases/download/v0.2.0/proctor_0.2.0_Darwin_x86_64.tar.gz"
-  version "v0.2.0"
-  sha256 "a6a0411019a886b11328fb37a9ddd44c5b50d2c7f803229c72e44850e39b8a00"
+  url "https://github.com/gojektech/proctor/releases/download/v0.3.0/proctor_0.3.0_Darwin_x86_64.tar.gz"
+  version "v0.3.0"
+  sha256 "87b3a90b67b6a6c90c9020ee2c59bb18f541fd2920e4fdabade0349db5b71e0f"
 
   bottle :unneeded
 
