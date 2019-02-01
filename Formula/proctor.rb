5df77b1a43beb75713db3cff09ea204521704b7a
Travis CI User <travis@example.org>
2019-02-01 12:08:48 +0000

    [TravisCI] updating brew formula for release v0.5.0

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -1,9 +1,9 @@
 class Proctor < Formula
   desc "Proctor CLI"
   homepage "https://github.com/gojektech/proctor"
-  url "https://github.com/gojektech/proctor/releases/download/v0.4.0/proctor_0.4.0_Darwin_x86_64.tar.gz"
-  version "0.4.0"
-  sha256 "c3afe46a05b6ac1c828699fff4c97d49340efa17baeb80ed43c13e13854c2f08"
+  url "https://github.com/gojektech/proctor/releases/download/v0.5.0/proctor_0.5.0_Darwin_x86_64.tar.gz"
+  version "0.5.0"
+  sha256 "36e94761b3823db88b2c61160811f97970401c59c1e1006c5a08c974a2ee3e51"
   head "https://github.com/gojektech/proctor.git"
 
   bottle :unneeded
