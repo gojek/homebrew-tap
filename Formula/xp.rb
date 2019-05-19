fbd0c1ebfce1a9e3ed78e23b8182bbacc66e7f22
Karan Misra <karan.misra@go-jek.com>
2019-05-19 06:13:19 +0530

    Brew formula update for xp version v0.3.2

--- Formula/xp.rb
+++ Formula/xp.rb
@@ -2,9 +2,9 @@
 class Xp < Formula
   desc "extreme programming made simple"
   homepage "https://github.com/gojek/xp"
-  url "https://github.com/gojek/xp/releases/download/v0.3.1/xp_0.3.1_Darwin_x86_64.tar.gz"
-  version "0.3.1"
-  sha256 "87a836b4695d22425089e7bb7eba93933f50b8e5e7b82debd1cf02ba441fc35c"
+  url "https://github.com/gojek/xp/releases/download/v0.3.2/xp_0.3.2_Darwin_x86_64.tar.gz"
+  version "0.3.2"
+  sha256 "6c19545498028eb0c0dc781a62f6998c92e7e21dda9bd5fd2d089dd8d417eb8b"
 
   def install
     bin.install "xp"
